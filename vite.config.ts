656e285fe4151a14f16cd5aca4a89fa34cc14cf6
shafi- <shafi.cse.buet@gmail.com>
2026-09-10 01:06:12 +0600

    cloudflare config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
     react(),
+    cloudflare(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg'],
